d85eb7846d3a194a8cbbd9200a2bb4d6d191993f
Leslie Castelan Chavez <lesliecastelanchavez@gmail.com>
2023-10-22 19:48:47 -0700

    third test

--- pipe.c
+++ pipe.c
@@ -3,36 +3,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <errno.h> 
 
-int errno; 
+//int errno; 
 int main(int argc, char *argv[])
 {
 	// TODO: it's all yours
 
-	if(argc == 0) //if no args exit with errno EINVAL
+	if(argc < 2) //if no args exit with errno EINVAL
 	{
-		errno = 22; 
-		fprintf(stderr,"errno %di\n", errno);
-		printf("No args specified \n"); 
-		exit(EXIT_FAILURE); 
+		fprintf(stderr, "No args specified \n"); 
+		exit(EINVAL); //exit with error invalid argument 
 	}
 
 
-		
-
-
-	printf("how many args are there: %d\n", argc-1);
-	printf("what they are:\n");
-	for(int i=1; i <argc; i++) {
-		printf("argv %d is: %s \n", i, argv[i]);
-	}
-
-
-	//Need a way to vet the arguments typed by user are
-	//valid args? How could we check this
-
-
-	int pipefd[2]; 
 
 	//create pipe array
 	//pipe the array
@@ -42,28 +26,81 @@ int main(int argc, char *argv[])
 	//therefore, all parents (later args) wait for first
 	//might have to do recursion
 
-	int return_code = fork(); 
+	//create file I/O descriptor arrays to store output between 
+	//processes
+	int fds_prev[2];
+	int fds_next[2]; 
+	
+	//loop through all arguments
+	for(int i=1; i < argc; i++) {
+		
+	 
+		//will pipe until hits last argument/process
+		if(i < argc-1) {
+			pipe(fds_next);
+		}
+		//create parent/child process
+		int return_code = fork(); 
 
-	if(return_code == 0) {
-		printf("This is child process!\n");
-		execlp(argv[1], argv[1], NULL); //pass in child argument based on input args
-		//how to pass in diff args based on varying arguements?
-		//how to handle flag arguments? are we supposed to
+		//if in parent process 
+		if(return_code > 0) {
 
-		exit(0); //child exits
+			//if processes exist before it
+			if(i != 1) 
+			{
+				//error checking
+				if(close(fds_prev[0]) || close(fds_prev[1]) == -1 ) 
+				{
+					perror("Error in closing parent process: closing files");
+					return errno; 
+				}
+
+			}
+			
+			//if first or processes exist after it, i.e. more command line args
+			//copy file descriptors over to save them for child's input 
+			if(i < argc - 1) {
+				fds_prev[0] = fds_next[0];
+				fds_prev[1] = fds_next[1]; 
+
+			}
+
+
+			//wait for child
+			int pid = return_code; //until child w/ this pid returns, wait
+			int status = 0;
+			waitpid(pid, &status,0); 
+
+			printf("child exits with code %d\n", WEXITSTATUS(status)); 
+			if( WEXITSTATUS(status) != 0) 
+			{
+				printf("Child failed!");
+				exit(errno); //??? how to exit with proper code
+			}
+			
+		}
+
+		else if(return_code == 0) {
+			//printf("This is child process!\n");
+			execlp(argv[1], argv[1], NULL); 
+
+			//child passes output to buffer pipe
+			//how do we count bytes performed by ls, cat and wc
+			//write(fds[1], "", );
+			perror("execlp");
+			return 1;
+			exit(0); //child exits
+		}
+
+
+		else {
+			printf("Child process creation error!\n"); 
+		}
+	
+	
+	
 	}
-	else if(return_code > 0) {
-		printf("In parent process rn: \n");
-		int pid = return_code; //until child w/ this pid returns, wait
-		int status = 0;
-		waitpid(pid, &status,0); 
-
-		printf("child exits with code %d\n", WEXITSTATUS(status)); 
-	}
-
-	else {
-		printf("Child process creation error!\n"); 
-	}
+	
 
 	printf("Finished!\n"); 
 	return 0;
